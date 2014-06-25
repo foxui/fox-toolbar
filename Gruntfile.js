e8bed1feb15b7df827a7aef68e73be7ba5fa1089
miller <chen.minliang@gmail.com>
2014-06-25 13:50:35 +0800

    统一Gruntfile.js;样式内联;删除多余依赖;

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,3 @@
-
 /* jshint node: true */
 module.exports = function(grunt) {
   'use strict';
@@ -16,7 +15,7 @@ module.exports = function(grunt) {
 
     // Metadata.
     meta: {
-      distPath:       'dist/',
+      distPath: 'dist/',
       srcPath: 'src/'
     },
 
@@ -24,7 +23,7 @@ module.exports = function(grunt) {
             ' * =====================================================\n' +
             ' * Foxui v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
             ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' * Licensed under <%= pkg.license %> (https://github.com/foxui/fox-toolbar/blob/master/LICENSE)\n' +
+            ' * Licensed under <%= pkg.license %> (https://github.com/foxui/<%= pkg.name %>/blob/master/LICENSE)\n' +
             ' *\n' +
             ' * v<%= pkg.version %> designed by @fex-team.\n' +
             ' * =====================================================\n' +
@@ -34,44 +33,28 @@ module.exports = function(grunt) {
       dist: ['<%= meta.distPath %>']
     },
 
-    concat: {
-      foxui: {
-        options: {
-          banner: '<%= banner %>'
-        },
-        src: [
-          'src/*.js',
-        ],
-        dest: '<%= meta.distPath %><%= pkg.name %>.js'
-      }
-    },
-
     sass: {
       options: {
         banner: '<%= banner %>',
         style: 'expanded',
         unixNewlines: true
       },
-      dist: {
+      dev: {
         files: {
-          '<%= meta.distPath %><%= pkg.name %>.css': 'sass/fox-toolbar.scss'
-        }
-      }
-    },
-
-    csscomb: {
-      options: {
-        config: 'sass/.csscomb.json'
-      },
-      dist: {
-        files: {
-          '<%= meta.distPath %><%= pkg.name %>.css': '<%= meta.distPath %>/<%= pkg.name %>.css'
+          '<%= meta.srcPath %>css/<%= pkg.name %>.css': 'sass/<%= pkg.name %>.scss'
         }
       }
     },
 
     copy: {
-      tags: {
+      css: {
+        expand: true,
+        cwd: 'src/css',
+        src: '**/*',
+        dest: '<%= meta.distPath %>css/'
+      },
+
+      src: {
         expand: true,
         cwd: 'src/',
         src: '*.html',
@@ -85,36 +68,19 @@ module.exports = function(grunt) {
         keepSpecialComments: '*' // set to '*' because we already add the banner in sass
       },
       foxui: {
-        src: '<%= meta.distPath %><%= pkg.name %>.css',
-        dest: '<%= meta.distPath %><%= pkg.name %>.min.css'
-      }
-    },
-
-    uglify: {
-      options: {
-        banner: '<%= banner %>',
-        compress: true,
-        mangle: true,
-        preserveComments: false
-      },
-      foxui: {
-        src: '<%= concat.foxui.dest %>',
-        dest: '<%= meta.distPath %><%= pkg.name %>.min.js'
+        src: '<%= meta.distPath %>css/<%= pkg.name %>.css',
+        dest: '<%= meta.distPath %>css/<%= pkg.name %>.min.css'
       }
     },
 
     watch: {
       scripts: {
         files: [
-          'sass/*.scss',
-          'src/*.html'
+          'sass/*',
+          'src/**/*'
         ],
         tasks: ['dist']
       }
-    },
-
-    qunit: {
-      files: ['test/test.html']
     }
   });
 
@@ -123,11 +89,9 @@ module.exports = function(grunt) {
   require('time-grunt')(grunt);
 
   // Default task(s).
-  grunt.registerTask('dist-css', ['sass', 'csscomb', 'cssmin']);
-  grunt.registerTask('dist-js', ['concat', 'uglify']);
-  grunt.registerTask('dist', ['clean', 'dist-css', 'dist-js', 'copy']);
+  grunt.registerTask('dist-css', ['sass', 'copy:css', 'cssmin']);
+  grunt.registerTask('dist', ['clean', 'dist-css', 'copy:src']);
   grunt.registerTask('build', ['dist']);
   grunt.registerTask('default', ['dist']);
-  grunt.registerTask('test', ['dist', 'qunit']);
 
 };
--- dist/fox-toolbar.js
+++
@@ -1,9 +0,0 @@
-/*!
- * =====================================================
- * Foxui v0.1.0 (https://github.com/foxui/fox-toolbar)
- * Copyright 2014 fex-team
- * Licensed under BSD (https://github.com/foxui/fox-toolbar/blob/master/LICENSE)
- *
- * v0.1.0 designed by @fex-team.
- * =====================================================
- */
